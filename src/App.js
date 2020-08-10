6e8c54c8426eeeeda0bffcab10172862b19ab20d
Jonifwun <jonathanphilipday@gmail.com>
2020-08-10 09:52:54 -0400

    convert to classes and add drug search term to state

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+class App extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      drugSearchTerm: ''
+    }
+  }
+
+  changeDrugSearchTerm = (event) => {
+    event.preventDefault();
+
+    this.setState({
+      drugSearchTerm: event.target.value
+    });
+
+  }
+
+  render(){
+
   return (
     <div className="App">
       <div class="container">
@@ -10,7 +28,7 @@ function App() {
       <h1>Wiki Drug Search</h1>
       <div id="searchForm" class="input-field">
     <form>
-      <input id="searchTerm" class="materialize-textarea" name="search" placeholder="Search By Drug Name" autofocus></input>
+      <input id="searchDrug" class="materialize-textarea" value={this.state.drugSearchTerm || ''} onChange={this.changeDrugSearchTerm} name="search" placeholder="Search By Drug Name" autofocus></input>
       <div class="row">
         <div class="col s12 m4">
           <button id="searchButt" class="btn waves-effect waves-light " type="submit" name="action"><i class="material-icons left">search</i>Submit</button>
@@ -31,6 +49,7 @@ function App() {
   </div>
     </div>
   );
+}
 }
 
 export default App;
